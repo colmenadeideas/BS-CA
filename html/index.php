139f87cffa4e62537698733f9120647e65e8a1f7
bs-gruiz <gruiz@besign.com.ve>
2015-01-19 14:45:40 -0430

    n

--- html/index.php
+++ html/index.php
@@ -1,6 +1,6 @@
 <?php
 	
-	error_reporting(E_ALL);
+	//error_reporting(E_ALL);
 	
 	require_once ('../includes/config/local.php'); //if Production Site change to server.php
 	require_once ('../includes/config/config.php');
--- includes/config/local.php
+++ includes/config/local.php
@@ -1,6 +1,6 @@
 <?php
 		//Path Info
-		define ('URL', "http://localhost/BS-CA/html/"); 
+		define ('URL', "http://localhost/CapitanAmerica/BS-CA/html/"); 
 
 		//Server Info
 		define ( 'DB_SERVER', 'localhost');
