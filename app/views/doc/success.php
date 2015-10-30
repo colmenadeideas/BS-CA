0f75a98ae107e7240f0e9e49df4c7e76adcfd56d
nuzzolillo <andredavid@besign.com.ve>
2015-10-30 17:15:04 -0430

    success

--- app/views/doc/success.php
+++ app/views/doc/success.php
@@ -1,52 +1,42 @@
+<div class="container-fluid success">
+	<div class="col-md-12" style="color:white;">
+		<h2 class="text-center" style="margin:50px 0 20px;"> <span><i class="glyphicon glyphicon-ok success-ok" style="border: solid 2px #3ED87A;padding: 12px;border-radius: 50%;margin-right: 25px;color: #3ED87A;"></i></span>SU PAGO SE HA REALIZADO CON EXITO</h2>
+	</div>
+	<div class="col-md-4 col-md-offset-2 success-resume">
+		<div class="col-md-12 text-center">
+			<h3>DATOS DE PAGO</h3>
+		</div>
+		<div class="col-md-12 success-item">
+			<div class="col-md-12">
+				<h4 class="left">Nombre de Doctor:</h4>
+				<h4 class="right text-right">Maria Hurtado Perez</h4>
+			</div>
+			<div class="col-md-12">
+				<h4 class="left">Razon de la consulta:</h4>
+				<h4 class="right text-right">Analisis de sangre</h4>
+			</div>
+			<div class="col-md-12">
+				<h4 class="left">Fecha:</h4>
+				<h4 class="right text-right">5/12/2015</h4>
+			</div>
+			<div class="col-md-12">
+				<h4 class="left">Hora:</h4>
+				<h4 class="right text-right">4:20 PM</h4>
+			</div>
+		</div>
+		<div class="col-md-12 success-amount">
+			<h3 class="left">TOTAL</h3>
+			<h3 class="right">250 BsF</h3>
+		</div>
+	</div>
+	<div class="col-md-4" style="color:white">
+		<div class="col-md-12">
+			<div class="col-md-10 col-md-offset-1 text-center" style="margin-top:15px;">
+				<h3 style="font-weight:300;margin-bottom:10px;">Gracias por preferir OKIDOC, la comunidad mas grande de Venezuela</h3>
+				<p>No sé cómo podré agradecerte este favor; las plantas agradecerán tus cuidados; el calzado debe ser, esencialmente, cómodo: sus pies se lo agradecerán</p>
+				<button class="btn btn-green right" style="background-color:#3ED87A;color:white; padding: 20px;border-radius: 30px;width: 80%;text-align: center;font-size: 20px;text-transform: uppercase;margin: 10px 10%;"><a href="#">Agergar otra cita</a></button>
 
-<div class="col-md-12">
-	<h2 class="text-center">SU PAGO SE HA REALIZADO CON EXITO</h2>
-</div>
-<div class="col-md-4 col-md-offset-2 success-resume">
-	<div class="col-md-12 success-item">
-		<div class="col-md-12">
-			<h4 class="left">Nombre de Doctor:</h4>
-			<h4 class="right text-right">Maria Hurtado Perez</h4>
+			</div>
 		</div>
-		<div class="col-md-12">
-			<h4 class="left">Razon de la consulta:</h4>
-			<h4 class="right text-right">Analisis de sangre</h4>
-		</div>
-		<div class="col-md-12">
-			<h4 class="left">Fecha:</h4>
-			<h4 class="right text-right">5/12/2015</h4>
-		</div>
-		<div class="col-md-12">
-			<h4 class="left">Hora:</h4>
-			<h4 class="right text-right">4:20 PM</h4>
-		</div>
-	</div>
-	<div class="col-md-12 success-amount">
-		<h3 class="left">TOTAL</h3>
-		<h3 class="right">250 BsF</h3>
-	</div>
-</div>
-<div class="col-md-4 col-md-offset-1 success-resume">
-	<div class="col-md-12 success-item">
-		<div class="col-md-12">
-			<h4 class="left">Nombre de Doctor:</h4>
-			<h4 class="right text-right">Maria Hurtado Perez</h4>
-		</div>
-		<div class="col-md-12">
-			<h4 class="left">Razon de la consulta:</h4>
-			<h4 class="right text-right">Analisis de sangre</h4>
-		</div>
-		<div class="col-md-12">
-			<h4 class="left">Fecha:</h4>
-			<h4 class="right text-right">5/12/2015</h4>
-		</div>
-		<div class="col-md-12">
-			<h4 class="left">Hora:</h4>
-			<h4 class="right text-right">4:20 PM</h4>
-		</div>
-	</div>
-	<div class="col-md-12 success-amount">
-		<h3 class="left">TOTAL</h3>
-		<h3 class="right">250 BsF</h3>
 	</div>
 </div>
